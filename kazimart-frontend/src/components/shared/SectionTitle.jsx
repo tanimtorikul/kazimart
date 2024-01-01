fd876308094fca11281ea41024d4829409b8146b
tanimtorikul <torikulswe@gmail.com>
2024-01-01 23:38:05 +0600

    modified

--- kazimart-frontend/src/components/shared/SectionTitle.jsx
+++ kazimart-frontend/src/components/shared/SectionTitle.jsx
@@ -1,12 +1,10 @@
-
-
-const SectionTitle = ({heading, subHeading}) => {
-    return (
-        <div>
-            <h2>{heading}</h2>
-            <p>{subHeading}</p>
-        </div>
-    );
+const SectionTitle = ({ heading, subHeading }) => {
+  return (
+    <div>
+      <p>{subHeading}</p>
+      <h2>{heading}</h2>
+    </div>
+  );
 };
 
 export default SectionTitle;

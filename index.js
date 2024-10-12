c4b559669bc04d2f1ba1257362ddd8343ba1f509
tanimtorikul <torikulswe@gmail.com>
2024-10-12 16:12:29 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
-      "Pinged your deployment. successfully connected to MongoDB!"
+      "Pinged your deployment, connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error

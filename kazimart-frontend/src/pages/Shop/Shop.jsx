abd5f42c70f8f9e047ddff4bd070baabbce1dd8f
tanimtorikul <torikulswe@gmail.com>
2024-10-12 13:22:07 +0600

    implemented shop page

--- kazimart-frontend/src/pages/Shop/Shop.jsx
+++ kazimart-frontend/src/pages/Shop/Shop.jsx
@@ -1,20 +1,26 @@
+import Item from "../../components/shared/Item";
+import useProducts from "../../hooks/useProducts";
 
 const Shop = () => {
-    return (
-        <div>
-              <div className="max-w-[1400px] mx-auto bg-teal-500 my-2 py-24 text-white">
-        <h2 className="capitalize text-2xl font-bold text-center">
-          Shop
-        </h2>
+  const [items] = useProducts();
+  return (
+    <div>
+      <div className="max-w-[1400px] mx-auto bg-teal-500 my-2 py-24 text-white">
+        <h2 className="capitalize text-2xl font-bold text-center">Shop</h2>
         <p className="my-4 text-center">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
           officia sapiente nostrum magni reprehenderit, modi eius consequuntur
           neque aperiam eos!
         </p>
       </div>
-            
-        </div>
-    );
+      <div></div>
+      <div className="max-w-[1400px] mx-auto grid grid-cols-2 md:grid-cols-6 gap-4">
+        {items.map((item) => (
+          <Item key={item.id} item={item}></Item>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
